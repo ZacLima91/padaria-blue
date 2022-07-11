b9dc98da15d9463aa8985a3cabd58459db057791
zaclima <jadielsonmanoel@gmail.com>
2022-07-11 11:46:34 -0300

    feature: created the id error method at the categories services

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { handleConstrainUniqueError } from 'src/utils/handle-error-unique.util';
 import { CreateCategoryDto } from './dto/create-category.dto';
@@ -18,17 +18,31 @@ export class CategoriesService {
     return this.prisma.category.findMany();
   }
 
+  async verifyIdAndReturnCategory(id: string): Promise<Category> {
+    const category: Category = await this.prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Entrada de id ${id} não encontrada`);
+    }
+
+    return category;
+  }
+
   findOne(id: string): Promise<Category> {
-    return this.prisma.category.findUnique({ where: { id } });
+    return this.verifyIdAndReturnCategory(id);
   }
 
   async update(id: string, dto: UpdateCategoryDto): Promise<Category> {
+    await this.verifyIdAndReturnCategory(id);
     return this.prisma.category
       .update({ where: { id }, data: dto })
       .catch(handleConstrainUniqueError);
   }
 
-  remove(id: string) {
+  async remove(id: string) {
+    await this.verifyIdAndReturnCategory(id);
     return this.prisma.category.delete({
       where: { id },
       select: { name: true },
